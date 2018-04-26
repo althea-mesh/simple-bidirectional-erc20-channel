1e650049d709570707b36f621266e322cfb3d79b
Rahul <rksethuram9@gmail.com>
2018-04-25 18:01:17 -0700

    ERC20

--- contracts/ChannelManager.sol
+++ contracts/ChannelManager.sol
@@ -1,6 +1,7 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
 import "./ECTools.sol";
+import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract ChannelManager {
     event ChannelOpen(
@@ -8,6 +9,8 @@ contract ChannelManager {
         address indexed agentA,
         address indexed agentB,
         uint256 depositA,
+        address[] tokenContracts,
+        uint[] tokenAmounts,
         uint256 challenge
     );
     event ChannelJoin(
@@ -15,7 +18,9 @@ contract ChannelManager {
         address indexed agentA,
         address indexed agentB,
         uint256 depositA,
-        uint256 depositB
+        uint256 depositB,
+        address[] tokenContracts,
+        uint[] tokenAmounts
     );
     event ChannelChallenge(
         bytes32 indexed channelId,
@@ -23,7 +28,11 @@ contract ChannelManager {
     );
     event ChannelUpdateState(
         bytes32 indexed channelId,
-        uint256 nonce
+        uint256 nonce,
+        uint256[] tokenBalanceA,
+        uint256[] tokenBalanceB,
+        uint256 balanceA,
+        uint256 balanceB
     );
     event ChannelClose(bytes32 indexed channelId);
 
@@ -39,41 +48,70 @@ contract ChannelManager {
         address agentB;
         uint depositA; // Deposit of agent A
         uint depositB; // Deposit of agent B
+        address[] tokensHeld;
+        mapping (address => uint) tokenDepositA;
+        mapping (address => uint) tokenDepositB;
         ChannelStatus status; 
         uint challenge;
         uint nonce;
         uint closeTime;
         uint balanceA; // for state update
         uint balanceB; // for state update
+        mapping (address => uint) tokenBalanceA;
+        mapping (address => uint) tokenBalanceB;
     }
 
     mapping (bytes32 => Channel) public channels;
     mapping (address => mapping(address => bytes32)) public activeIds;
 
-    function openChannel(address to, uint challenge) payable public {
-        require(challenge != 0); // need to have challenge period
-        require(to != address(0)); // need counterparty address
-        require(to != msg.sender); // cant create with yourself
-        require(activeIds[msg.sender][to] == bytes32(0)); // if channel is already open dont create
-        require(activeIds[to][msg.sender] == bytes32(0)); // dont allow to open both ways
+    function openChannel(
+        address to,
+        uint challenge,
+        address[] tokenContracts,
+        uint[] tokenAmounts
+    ) 
+        payable
+        public 
+    {
+        require(challenge != 0, "Challenge period must be non-zero.");
+        require(to != address(0), "Need counterparty address.");
+        require(to != msg.sender, "Cannot create channel with yourself.");
+        require(activeIds[msg.sender][to] == bytes32(0), "Cannot create multiple channels with counterparty.");
+        require(activeIds[to][msg.sender] == bytes32(0), "Cannot create multiple channels with counterparty.");
 
         bytes32 id = keccak256(msg.sender, to, now);
-        Channel memory channel;
+        Channel storage channel = channels[id];
         channel.agentA = msg.sender;
         channel.agentB = to;
         channel.depositA = msg.value;
         channel.balanceA = msg.value; // so that close can happen without any state updates
+
+        for (uint i = 0; i < tokenContracts.length; i++) {
+            ERC20 erc20 = ERC20(tokenContracts[i]);
+            require(erc20.transferFrom(msg.sender, address(this), tokenAmounts[i]), "Could not transfer tokens."); // must be approved to send to this address
+            channel.tokenDepositA[address(tokenContracts[i])] = tokenAmounts[i];
+            channel.tokenBalanceA[address(tokenContracts[i])] = tokenAmounts[i];
+            channel.tokensHeld.push(tokenContracts[i]);
+        }
+
         channel.status = ChannelStatus.Open;
         channel.challenge = challenge;
 
-        // save to state
-        channels[id] = channel;
         // Add it to the lookup table
         activeIds[msg.sender][to] = id;
-        emit ChannelOpen(id, channel.agentA, channel.agentB, channel.depositA, channel.challenge);
+
+        emit ChannelOpen(
+            id,
+            channel.agentA,
+            channel.agentB,
+            channel.depositA,
+            tokenContracts,
+            tokenAmounts,
+            channel.challenge
+        );
     }
 
-    function joinChannel(bytes32 id) payable public {
+    function joinChannel(bytes32 id, address[] tokenContracts, uint[] tokenAmounts) payable public {
         Channel storage channel = channels[id];
 
         require(msg.sender == channel.agentB);
@@ -82,13 +120,32 @@ contract ChannelManager {
         channel.depositB = msg.value;
         channel.balanceB = msg.value;
 
+        for (uint i = 0; i < tokenContracts.length; i++) {
+            require(tokenContracts[i] == channel.tokensHeld[i]);
+
+            ERC20 erc20 = ERC20(tokenContracts[i]);
+            require(erc20.transferFrom(msg.sender, address(this), tokenAmounts[i])); // must be approved to send to this address
+            channel.tokenDepositB[address(tokenContracts[i])] = tokenAmounts[i];
+            channel.tokenBalanceB[address(tokenContracts[i])] = tokenAmounts[i];
+        }
+
         channel.status = ChannelStatus.Joined;
-        emit ChannelJoin(id, channel.agentA, channel.agentB, channel.depositA, channel.depositB);
+        emit ChannelJoin(
+            id,
+            channel.agentA,
+            channel.agentB,
+            channel.depositA,
+            channel.depositB,
+            tokenContracts,
+            tokenAmounts
+        );
     }
 
     function isValidStateUpdate(
         bytes32 channelId,
         uint256 nonce,
+        uint256[] tokenBalanceA,
+        uint256[] tokenBalanceB,
         uint256 balanceA,
         uint256 balanceB,
         string sigA,
@@ -100,9 +157,20 @@ contract ChannelManager {
         view
         returns (bool)
     {
-        Channel memory channel = channels[channelId];
+        Channel storage channel = channels[channelId];
 
-        require(balanceA + balanceB == channel.depositA + channel.depositB); // balances must add up to deposit
+        // balances must add up to deposit
+        require(balanceA + balanceB == channel.depositA + channel.depositB); // eth
+        for (uint i = 0; i < channel.tokensHeld.length; i++) {
+            // tokens
+            // balances must be passed in with same order as tokensHeld
+            require(
+                tokenBalanceA[i] + tokenBalanceB[i] ==
+                    channel.tokenDepositA[channel.tokensHeld[i]] +
+                        channel.tokenDepositB[channel.tokensHeld[i]]
+            );
+        }
+
         require(channel.status == ChannelStatus.Joined || channel.status == ChannelStatus.Challenge); // channel must be joined
 
         // require state info to be signed by both participants
@@ -110,7 +178,15 @@ contract ChannelManager {
         // https://medium.com/metamask/scaling-web3-with-signtypeddata-91d6efc8b290
         // https://github.com/ukstv/sign-typed-data-test/blob/master/contracts/SignTypedData.sol#L11
         // https://github.com/MetaMask/eth-sig-util/blob/master/index.js
-        bytes32 fingerprint = keccak256(channelId, nonce, balanceA, balanceB);
+        bytes32 fingerprint = keccak256(
+            channelId,
+            nonce,
+            tokenBalanceA,
+            tokenBalanceB,
+            balanceA,
+            balanceB
+        );
+
         bytes32 signTypedDataFingerprint = keccak256(
             keccak256("bytes32 hash"),
             keccak256(fingerprint)
@@ -131,6 +207,8 @@ contract ChannelManager {
     function updateState(
         bytes32 channelId,
         uint256 nonce,
+        uint256[] tokenBalanceA,
+        uint256[] tokenBalanceB,
         uint256 balanceA,
         uint256 balanceB,
         string sigA,
@@ -142,18 +220,32 @@ contract ChannelManager {
 
         // sanity checks
         require(msg.sender == channel.agentA || msg.sender == channel.agentB); // comes from agent
-        require(channel.status != ChannelStatus.Closed); // channel open or challenge status
         require(
-            isValidStateUpdate(channelId, nonce, balanceA, balanceB, sigA, sigB, true, true) == true
+            isValidStateUpdate(
+                channelId,
+                nonce,
+                tokenBalanceA,
+                tokenBalanceB,
+                balanceA,
+                balanceB,
+                sigA,
+                sigB,
+                true,
+                true
+            ) == true
         ); // valid signatures from both parties
         require(nonce > channel.nonce); // need a higher sequence update
 
         // set state variables
         channel.balanceA = balanceA;
         channel.balanceB = balanceB;
+        for (uint i = 0; i < channel.tokensHeld.length; i++) {
+            channel.tokenBalanceA[channel.tokensHeld[i]];
+            channel.tokenBalanceB[channel.tokensHeld[i]];
+        }
         channel.nonce = nonce;
 
-        emit ChannelUpdateState(channelId, nonce);
+        emit ChannelUpdateState(channelId, nonce, tokenBalanceA, tokenBalanceB, balanceA, balanceB);
     }
 
     function startChallenge(bytes32 channelId) public {
@@ -170,12 +262,8 @@ contract ChannelManager {
         emit ChannelChallenge(channelId, channel.closeTime);
     }
 
-    function closeChannel(
-        bytes32 channelId
-    )
-        public
-    {
-        Channel memory channel = channels[channelId];
+    function closeChannel(bytes32 channelId) public {
+        Channel storage channel = channels[channelId];
 
         // request must come from agents
         require(msg.sender == channel.agentA || msg.sender == channel.agentB);
@@ -188,13 +276,30 @@ contract ChannelManager {
         channel.agentA.transfer(channel.balanceA);
         channel.agentB.transfer(channel.balanceB);
 
-        delete channels[channelId];
-        delete activeIds[channel.agentA][channel.agentB];
+        for (uint i = 0; i < channel.tokensHeld.length; i++) {
+            ERC20 token = ERC20(channel.tokensHeld[i]);
+            require(
+                token.transfer(
+                    channel.agentA,
+                    channel.tokenBalanceA[channel.tokensHeld[i]]
+                )
+            );
+            require(
+                token.transfer(
+                    channel.agentB,
+                    channel.tokenBalanceB[channel.tokensHeld[i]]
+                )
+            );
+        }
+
+        channel.status = ChannelStatus.Closed;
+
+        // cannot delete channel anymore in order to be able to withdraw tokens
 
         emit ChannelClose(channelId);
     }
 
-    function getChannel(bytes32 id) public view returns(
+    function getChannel(bytes32 id) public view returns (
         address,
         address,
         uint,
@@ -218,6 +323,33 @@ contract ChannelManager {
             channel.closeTime,
             channel.balanceA,
             channel.balanceB
+        );
+    }
+
+    function getChannelTokenDetails(bytes32 channelId) public view returns (
+        address[],
+        uint[],
+        uint[],
+        uint[],
+        uint[]
+    ) {
+        Channel storage channel = channels[channelId];
+        uint[] memory tokenDepositA = new uint[](channel.tokensHeld.length);
+        uint[] memory tokenDepositB = new uint[](channel.tokensHeld.length);
+        uint[] memory tokenBalanceA = new uint[](channel.tokensHeld.length);
+        uint[] memory tokenBalanceB = new uint[](channel.tokensHeld.length);
+        for (uint i = 0; i < channel.tokensHeld.length; i++) {
+            tokenDepositA[i] = channel.tokenDepositA[channel.tokensHeld[i]];
+            tokenDepositB[i] = channel.tokenDepositB[channel.tokensHeld[i]];
+            tokenBalanceA[i] = channel.tokenBalanceA[channel.tokensHeld[i]];
+            tokenBalanceB[i] = channel.tokenBalanceB[channel.tokensHeld[i]];
+        }
+        return (
+            channel.tokensHeld,
+            tokenDepositA,
+            tokenDepositB,
+            tokenBalanceA,
+            tokenBalanceB
         );
     }
 }
