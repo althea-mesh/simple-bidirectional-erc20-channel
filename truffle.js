8871ab1f87b260f20a52e44a0867079fbe8ead7f
Rahul <rksethuram9@gmail.com>
2018-04-25 17:57:40 -0700

    INitial commit

---
+++ truffle.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
+
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 9545,
+      network_id: '*' // Match any network id
+    },
+    ganache: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(
+          process.env.ETH_KEY,
+          process.env.ETH_NODE_URL
+        )
+      },
+      network_id: 4
+    }
+  }
+}
