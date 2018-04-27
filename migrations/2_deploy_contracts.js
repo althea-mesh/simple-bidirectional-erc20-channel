a4449cde08605d561f73b0c3bea97bcef11e3ebc
Rahul <rksethuram9@gmail.com>
2018-04-26 17:24:49 -0700

    Add simpletoken for testing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 /* globals artifacts */
 const ChannelManager = artifacts.require('./ChannelManager.sol')
 const ECTools = artifacts.require('./ECTools.sol')
+const SimpleToken = artifacts.require('./SimpleToken.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(ECTools)
   deployer.link(ECTools, ChannelManager)
   deployer.deploy(ChannelManager)
+  deployer.deploy(SimpleToken)
 }
