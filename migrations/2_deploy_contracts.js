19100a6881fb9d459411f5281305446241b5580d
Rahul <rksethuram9@gmail.com>
2018-04-30 13:00:08 -0700

    Transfer tokens during migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,14 @@
-/* globals artifacts */
+/* globals artifacts, web3 */
 const ChannelManager = artifacts.require('./ChannelManager.sol')
 const ECTools = artifacts.require('./ECTools.sol')
 const SimpleToken = artifacts.require('./SimpleToken.sol')
 
-module.exports = function (deployer) {
+module.exports = (deployer, network, accounts) => {
   deployer.deploy(ECTools)
   deployer.link(ECTools, ChannelManager)
   deployer.deploy(ChannelManager)
-  deployer.deploy(SimpleToken)
+  deployer.deploy(SimpleToken).then(async () => {
+    const simpleToken = await SimpleToken.deployed()
+    simpleToken.transfer(accounts[1], web3.toWei(5000, 'ether'))
+  })
 }
