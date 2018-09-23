6037cd0b442208958f5d4425fbb2d0f32cd99028
Michał Papierski <michal@papierski.net>
2018-09-23 10:06:49 +0200

    Update dependencies.
    At least on my machine installation of "sha3" package failed because of
    C++ compile errors. After running `npm update` the problem was
    resolved. This commit also fixes a warning regarding zeppelin-solidity
    which was renamed.

--- contracts/ChannelManager.sol
+++ contracts/ChannelManager.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.23;
 
 import "./ECTools.sol";
-import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract ChannelManager {
     event ChannelOpen(
