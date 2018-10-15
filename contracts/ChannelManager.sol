3a988224bd2e04e4c9f8a6a18427cb6e0a73eaa2
Justin Kilpatrick <kilpatrickjustin@protonmail.ch>
2018-10-15 10:50:31 -0400

    Close channel before moving balances in order to prevent reentrancy

--- contracts/ChannelManager.sol
+++ contracts/ChannelManager.sol
@@ -318,10 +318,8 @@ contract ChannelManager {
         }
 
 
-        // TODO why do we transfer them nothing?
-        // zero out to avoid reentrancy
-        channel.balanceA = 0;
-        channel.balanceB = 0;
+        // prevents reentrancy
+        channel.status = ChannelStatus.Closed;
         // if true, then use final state to close channel
         if (channel.tokenContract == address(0)) {
             channel.agentA.transfer(channel.balanceA);
@@ -338,7 +336,9 @@ contract ChannelManager {
             );
         }
 
-        channel.status = ChannelStatus.Closed; // redundant bc channel is deleted
+        // TODO zeroing these is probably redundant
+        channel.balanceA = 0;
+        channel.balanceB = 0;
         delete activeIds[channel.agentA][channel.agentB][channel.tokenContract];
         delete channels[channelId];
 
