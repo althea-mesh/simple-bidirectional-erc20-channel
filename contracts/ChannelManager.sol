f92e8049286e2af14534c73406f24afe8c4f743f
Sebastian Bolanos <sebasheston@gmail.com>
2018-10-22 19:57:03 -0700

    Clean left over deps

--- contracts/ChannelManager.sol
+++ contracts/ChannelManager.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.23;
 
 import "./ECTools.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract ChannelManager {
     event ChannelOpen(
