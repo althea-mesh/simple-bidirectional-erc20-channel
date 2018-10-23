e89d746440554db5d21764a1fe4a4defbf5cf35d
Sebastian Bolanos <sebasheston@gmail.com>
2018-10-22 18:09:36 -0700

    Change constant address to checksum addresses

--- test/constants.js
+++ test/constants.js
@@ -1,15 +1,23 @@
+const e = require('ethers')
 module.exports = {
   ACCT_0_PRIVKEY:
     "0x1ec60acdff80be2b4943c26371aa3e04db5a024d036cc609399f2049c700e60d",
-  ACCT_0_ADDR: "0xd01a2a8a2151b819c50aec72742560f65682bb10",
+  ACCT_0_ADDR: web3.utils.toChecksumAddress(
+    "0xd01a2a8a2151b819c50aec72742560f65682bb10"
+  ),
 
   ACCT_1_PRIVKEY:
     "0x745c6c1818feee717976418d13bff3b50e39d43f49e8b055b2c184ef2951da5a",
-  ACCT_1_ADDR: "0xfdfa0aa3c3193485ff2aa08a43530d13fd16ed6a",
+  ACCT_1_ADDR: web3.utils.toChecksumAddress(
+    "0xfdfa0aa3c3193485ff2aa08a43530d13fd16ed6a"
+  ),
 
   ACCT_2_PRIVKEY:
     "0xb72f5915bdde39444fa21939e9edde0a777e955ed23647be766d452a45c5979c",
-  ACCT_2_ADDR: "0x944a4531ae61ed87190d567d7034456e173d94f5",
+  ACCT_2_ADDR: web3.utils.toChecksumAddress(
+    "0x944a4531ae61ed87190d567d7034456e173d94f5"
+  ),
+
   MNEMONIC:
     "cook mango twist then skin sort option civil have still rather guilt",
   CHANNEL_STATUS: {
